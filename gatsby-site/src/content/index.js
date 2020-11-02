90e7ac1e8c434cb6398ba04760a09c8b2ccc5d0b
Austen Turner <austenpturner@msn.com>
2020-11-01 20:49:57 -0800

    add CSS transitions and styles

---
+++ gatsby-site/src/content/index.js
@@ -0,0 +1,21 @@
+const indexContent = {
+    "headings": {
+        "h1": "Close Faster,",
+        "h2": "With Human Centered AI"
+    },
+    "links": [
+        {
+            "link-btn": "Schedule A Demo",
+            "link-btn": "About Us"
+        }
+    ],
+    "info-bullets": [
+        {
+            "bullet": "Get Their Attention",
+            "bullet": "Faster Excecution",
+            "bullet": "Artificial Intelligence"
+        }
+    ]
+};
+
+export default indexContent;
